c5f19dbe5ac570373d602a7ce7cb2317c225975f
myin <minyin142p@gmail.com>
2023-10-21 16:41:45 +0200

    enemy follow player

---
+++ godot/src/Enemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed := 50
+
+@onready var navigation_agent := $NavigationAgent2D
+
+func _ready():
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+
+func _process(delta):
+	navigation_agent.target_position = get_tree().get_first_node_in_group("Player").global_position
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	var new_velocity: Vector2 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * speed
+
+	velocity = new_velocity
+	move_and_slide()
