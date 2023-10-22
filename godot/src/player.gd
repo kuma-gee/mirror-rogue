6e823ba6dbca2822a85f9592a57708c3741fa33e
myin <minyin142p@gmail.com>
2023-10-22 11:56:16 +0200

    player dash

--- godot/src/player.gd
+++ godot/src/player.gd
@@ -6,6 +6,9 @@ signal died()
 @export var accel := 800
 @export var health := 50
 
+@export var dash_force := 600
+@export var dash_deaccel := 2000
+
 @export var projectile_scene: PackedScene
 
 @onready var input: PlayerInput = $Input
@@ -13,12 +16,16 @@ signal died()
 @onready var shot_point = $Hand/ShotPoint
 
 var thrown = false
+var dashing = false
 
 func _ready():
 	input.just_pressed.connect(_on_just_pressed)
 
 func _on_just_pressed(ev: InputEvent):
-	if ev.is_action_pressed("throw") and not thrown:
+	if ev.is_action_pressed("dash") and not dashing:
+		velocity = velocity.normalized() * dash_force
+		dashing = true
+	elif ev.is_action_pressed("throw") and not thrown:
 		thrown = true
 		var projectile = projectile_scene.instantiate()
 		projectile.global_position = shot_point.global_position
@@ -30,10 +37,15 @@ func _process(delta):
 	hand.global_rotation = Vector2.RIGHT.angle_to(global_position.direction_to(get_global_mouse_position()))
 
 func _physics_process(delta):
-	var motion_x = input.get_action_strength("move_right") - input.get_action_strength("move_left")
-	var motion_y = input.get_action_strength("move_down") - input.get_action_strength("move_up")
+	if dashing:
+		velocity = velocity.move_toward(Vector2.ZERO, dash_deaccel * delta)
+		if velocity.length() < 0.5:
+			dashing = false
+	else:
+		var motion_x = input.get_action_strength("move_right") - input.get_action_strength("move_left")
+		var motion_y = input.get_action_strength("move_down") - input.get_action_strength("move_up")
+		velocity = velocity.move_toward(Vector2(motion_x, motion_y) * speed, accel * delta)
 	
-	velocity = velocity.move_toward(Vector2(motion_x, motion_y) * speed, accel * delta)
 	move_and_slide()
 
 
