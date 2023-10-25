c8305371f9341d789c5e6d1c3a680ce9ca9340ac
myin <minyin142p@gmail.com>
2023-10-25 16:23:18 +0200

    add invincible time

--- godot/src/character/Hurtbox.gd
+++ godot/src/character/Hurtbox.gd
@@ -1,14 +1,22 @@
 class_name HurtBox
 extends Area2D
 
-@export var remove_on_hit = true
+@export var invincible_time := 0.0
+@export var remove_on_hit := true
 
 signal hit(dmg)
 signal hit_with(area)
 signal knockback(dir)
 
+var invincible := false
+
 func damage(dmg: int, hitbox: Node2D, knockback_force := 0):
+	if invincible:
+		return false
+	
+	invincible = true
 	hit.emit(dmg)
 	hit_with.emit(hitbox)
 	knockback.emit(hitbox.global_position.direction_to(global_position) * knockback_force)
+	get_tree().create_timer(invincible_time).timeout.connect(func(): invincible = false)
 	return remove_on_hit
